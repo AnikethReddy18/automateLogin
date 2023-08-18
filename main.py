64679c9f81b679b480f79cdb87534824b89609b3
sunny <sunnyrockzzz1811@gmail.com>
2023-08-18 18:00:04 +0530

    Revert "Updated GUI to accept dropdowns"
    This reverts commit d0f3eaef688a2a166e465112206a89c88de56543.

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import customtkinter as ct
 
+ct.set_default_color_theme("blue")
 
 
 class Gui(ct.CTk):
     def __init__(self):
         super().__init__()
         self.to_fill = None
-        self.name_form = None
-        self.to_select = None
-        self.name_dropdown = None
+        self.name = None
         self.row = 1
-        self.dic_form = {}
-        self.dic_dropdown = {}
+        self.dic = {}
 
         # Configure Window
         self.title("Automatic Web Filler")
@@ -42,8 +40,8 @@ class Gui(ct.CTk):
     def create_form(self):
         if self.drop.get() == "Form":
 
-            self.name_form = ct.CTkEntry(self, placeholder_text="Name of element", font=self.font)
-            self.name_form.grid(column=0, row=self.row, padx=12, pady=20)
+            self.name = ct.CTkEntry(self, placeholder_text="Name of element", font=self.font)
+            self.name.grid(column=0, row=self.row, padx=12, pady=20)
 
             self.to_fill = ct.CTkEntry(self, placeholder_text="Value to fill", font=self.font)
             self.to_fill.grid(column=1, row=self.row, padx=12, pady=20)
@@ -53,23 +51,13 @@ class Gui(ct.CTk):
             self.row += 1
 
         else:
-
-            self.name_dropdown = ct.CTkEntry(self, placeholder_text="Name of element", font=self.font)
-            self.name_dropdown.grid(column=0, row=self.row, padx=12, pady=20)
-
-            self.to_select = ct.CTkEntry(self, placeholder_text="Value to fill", font=self.font)
-            self.to_select.grid(column=1, row=self.row, padx=12, pady=20)
-
-            self.submit_button_all.grid(column=2, row=self.row + 1, pady=12)
-
-            self.row += 1
+            pass
 
     def load(self):
-        self.dic_form[self.name_form.get()] = self.to_fill.get()
-        self.dic_dropdown[self.name_dropdown.get()] = self.to_select.get()
+        self.dic[self.name.get()] = self.to_fill.get()
 
     def create_load_form(self):
-        if self.name_form is None:
+        if self.name is None:
             self.create_form()
 
         else:
@@ -78,8 +66,7 @@ class Gui(ct.CTk):
 
     def submit(self):
         self.load()
-        print(self.dic_form)
-        print(self.dic_dropdown)
+        print(self.dic)
 
 
 app = Gui()
