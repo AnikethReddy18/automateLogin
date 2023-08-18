11809d7ca4db7b3ea94dd7f936f348fd45c8797b
sunny <sunnyrockzzz1811@gmail.com>
2023-08-18 17:28:18 +0530

    Updated backend

--- automate.py
+++ automate.py
@@ -3,34 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
-from dotenv import load_dotenv
-from os import getenv
-
-
-class Automate:
-    def __init__(self, delay):
-        # Time delay for loading(in seconds)
-        self.delay = delay
 
 
 class Driver:
-    def __init__(self, url):
+    def __init__(self, url, load_time):
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=chrome_options)
         self.url = url
         self.driver.get(self.url)
-        time.sleep(3)
+        time.sleep(load_time)
 
     def form_fill(self, name, to_fill):
         form = self.driver.find_element(By.NAME, name)
         form.clear()
         form.send_keys(to_fill)
-# Testing is done here
 
-urle = "https://www.instagram.com/"
-namee = "username"
-to_fille = "hello"
-instagram = Driver(urle)
-instagram.form_fill(namee, to_fille)
-
+    def select_dropdown(self, name, value):
+        drop = Select(self.driver.find_element(By.NAME, name))
+        drop.select_by_value(value)
