df8a1495c87a87379f0b9c0820334a685195f966
sunny <sunnyrockzzz1811@gmail.com>
2023-08-18 18:38:18 +0530

    Updated GUI to accept dropdowns

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ class Gui(ct.CTk):
         self.name_form = None
         self.to_select = None
         self.name_selector = None
-        self.row = 1
+        self.row = 2
         self.dic_forms = {}
         self.dic_selectors = {}
+        self.previous_selection = None
 
         # Configure Window
         self.title("Automatic Web Filler")
-        self.geometry("500x500")
+        self.geometry("625x500")
 
         # Font
         self.font = ("Work Sans", 16)
 
-        # Selection Part
-
         # Selection Label
         select_label = ct.CTkLabel(self, text="Select the form type:  ", font=self.font)
+        warning_label = ct.CTkLabel(self, text="Only click create after filling the values", font=self.font)
 
         # Dropdown
         self.drop = ct.CTkOptionMenu(self, values=["Form", "Dropdown"], font=self.font, corner_radius=3)
@@ -36,9 +36,10 @@ class Gui(ct.CTk):
         self.submit_button_all = ct.CTkButton(self, text="Submit All", font=self.font, command=self.submit)
 
         # Display Selection Part
-        select_label.grid(column=0, row=0, padx=12)
-        self.drop.grid(column=1, row=0, padx=12)
-        create_button.grid(column=2, row=0, padx=12)
+        select_label.grid(column=0, row=1, padx=12)
+        warning_label.grid(column=1, row=0, sticky="n")
+        self.drop.grid(column=1, row=1, padx=12)
+        create_button.grid(column=2, row=1, padx=12)
 
     def create_form(self):
 
@@ -52,6 +53,8 @@ class Gui(ct.CTk):
 
         self.row += 1
 
+        self.previous_selection = "form"
+
     def create_selector(self):
 
         self.name_selector = ct.CTkEntry(self, placeholder_text="Name of element", font=self.font)
@@ -64,35 +67,34 @@ class Gui(ct.CTk):
 
         self.row += 1
 
-    def load_form(self):
-        self.dic_forms[self.name_form.get()] = self.to_fill.get()
+        self.previous_selection = "dropdown"
 
-    def load_selector(self):
-        self.dic_selectors[self.name_selector.get()] = self.to_select.get()
+    def load(self):
+
+        if self.previous_selection == "form":
+            self.dic_forms[self.name_form.get()] = self.to_fill.get()
+        else:
+            self.dic_selectors[self.name_selector.get()] = self.to_select.get()
 
     def create_load(self):
 
         state = self.drop.get()
-        name_form = self.name_form
-        name_selector = self.name_selector
-
-        if name_form is not None or name_selector is not None:
+        if not self.previous_selection:
             if state == "Form":
                 self.create_form()
-
             else:
                 self.create_selector()
 
         else:
+            self.load()
             if state == "Form":
                 self.create_form()
-
             else:
                 self.create_selector()
 
     def submit(self):
         self.load()
-        print(self.dic)
+        print(f"Forms: {self.dic_forms}\nSelectors={self.dic_selectors}")
 
 
 app = Gui()
