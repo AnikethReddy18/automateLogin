40d36154a96e6b0e612a94af55c9d3a01e5b0550
sunny <sunnyrockzzz1811@gmail.com>
2023-08-18 14:10:49 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from tkinter import *
 import customtkinter as ct
 
 ct.set_default_color_theme("blue")
@@ -7,11 +6,46 @@ ct.set_default_color_theme("blue")
 class Gui(ct.CTk):
     def __init__(self):
         super().__init__()
+        self.row = 1
 
         # Configure Window
         self.title("Automatic Web Filler")
         self.geometry("500x500")
 
+        # Font
+        self.font = ("Work Sans", 16)
+
+        # Selection Part
+
+        # Selection Label
+        select_label = ct.CTkLabel(self, text="Select the form type:  ", font=self.font)
+
+        # Dropdown
+        self.drop = ct.CTkOptionMenu(self, values=["Form", "Dropdown"], font=self.font, corner_radius=3)
+
+        # Selection Button
+        select_button = ct.CTkButton(self, text="Create", font=self.font, command=self.create_form)
+
+        # Display Selection Part
+        select_label.grid(column=0, row=0, padx=12)
+        self.drop.grid(column=1, row=0, padx=12)
+        select_button.grid(column=2, row=0, padx=12)
+
+    def create_form(self):
+        if self.drop.get() == "Form":
+            name = ct.CTkEntry(self, placeholder_text="Name of element", font=self.font)
+            name.grid(column=0, row=self.row, padx=12, pady=20)
+
+            to_fill = ct.CTkEntry(self, placeholder_text="Value to fill", font=self.font)
+            to_fill.grid(column=1, row=self.row, padx=12, pady=20)
+
+            submit_button = ct.CTkButton(self, text="Submit", font=self.font)
+            submit_button.grid(column=2, row=self.row, padx=12, pady=12)
+
+            self.row += 1
+
+        else:
+            pass
 
 
 app = Gui()
